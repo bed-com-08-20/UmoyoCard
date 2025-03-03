7dd9215347dae50ebc26ad0aff92d00b45b059e5
Twapalisha Mushani <bed-com-16-20@unima.ac.mw>
2025-03-03 21:51:30 +0200

    added functions to handle saving records locally using shared preference

--- lib/screens/blood_sugar_screen.dart
+++ lib/screens/blood_sugar_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:umoyocard/screens/home_screen.dart';
 
 class BloodSugarScreen extends StatefulWidget {
@@ -14,10 +17,31 @@ class _BloodSugarScreenState extends State<BloodSugarScreen> {
   List<Map<String, dynamic>> records = []; // Start with no records
   bool showAllRecords = false;
 
+  @override
+  void initState() {
+    super.initState();
+    _loadRecords();
+  }
+
   void _toggleShowAllRecords() {
     setState(() {
       showAllRecords = !showAllRecords;
     });
+  }
+
+  void _saveRecords() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('records', jsonEncode(records));
+  }
+
+  void _loadRecords() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? savedData = prefs.getString('records');
+    if (savedData != null) {
+      setState(() {
+        records = List<Map<String, dynamic>>.from(jsonDecode(savedData));
+      });
+    }
   }
 
   void _showAddRecordDialog() {
@@ -38,14 +62,37 @@ class _BloodSugarScreenState extends State<BloodSugarScreen> {
           ),
           ElevatedButton(
             onPressed: () {
-              setState(() {
-                records.insert(0, {
-                  'value': double.tryParse(valueController.text) ?? 0.0,
-                  'status': 'Normal',
-                  'date': DateTime.now().toIso8601String().substring(0, 10),
+              double value = double.tryParse(valueController.text) ?? 0.0;
+              String? status;
+
+              //determine status
+              if (value < 2.8) {
+                status = 'Below';
+              } else if (value >= 2.8 && value < 3.9) {
+                status = 'Low';
+              } else if (value >= 3.9 && value <= 5.6) {
+                status = 'Normal';
+              } else if (value >= 5.7 && value <= 6.9) {
+                status = 'Prediabetes';
+              } else if (value >= 7.0 && value >= 8.0) {
+                status = 'Diabetes';
+              }
+
+              if (status != null) {
+                setState(() {
+                  records.insert(0, {
+                    'value': value,
+                    'status': status,
+                    'date': DateTime.now().toIso8601String().substring(0, 10),
+                  });
+                  _saveRecords();
                 });
-              });
-              Navigator.pop(context);
+                Navigator.pop(context);
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(
+                        'Invalid value! Please enter a valid blood sugar level. ')));
+              }
             },
             child: Text('Add'),
           ),
@@ -64,54 +111,44 @@ class _BloodSugarScreenState extends State<BloodSugarScreen> {
       ),
       child: BarChart(
         BarChartData(
-          barTouchData: BarTouchData(
-            enabled: true,
-            allowTouchBarBackDraw:true
-          ),
+          barTouchData:
+              BarTouchData(enabled: true, allowTouchBarBackDraw: true),
           baselineY: 30,
           alignment: BarChartAlignment.spaceAround,
           titlesData: FlTitlesData(
-            show: true,
-            leftTitles: AxisTitles(
-              axisNameWidget: const Text("Blood sugar (mmol/L)"),
-              axisNameSize: 30,
-              sideTitles: SideTitles(
-                showTitles: true,
-                interval: 5,
-                maxIncluded: true,
-                minIncluded: true,                
-                getTitlesWidget: (value, meta) => Text(value.toInt().toString()),
+              show: true,
+              leftTitles: AxisTitles(
+                axisNameWidget: const Text("Blood sugar (mmol/L)"),
+                axisNameSize: 30,
+                sideTitles: SideTitles(
+                  showTitles: true,
+                  interval: 5,
+                  maxIncluded: true,
+                  minIncluded: true,
+                  getTitlesWidget: (value, meta) =>
+                      Text(value.toInt().toString()),
+                ),
               ),
-            ),
-            rightTitles: AxisTitles(
-              sideTitles: SideTitles(
+              rightTitles: AxisTitles(
+                  sideTitles: SideTitles(
                 showTitles: false,
                 maxIncluded: true,
                 minIncluded: true,
-                
-              )    
-            ),
-            topTitles: AxisTitles(
-              axisNameWidget: const Text("Blood Sugar Graph"),
-              axisNameSize:35,
-              sideTitles: SideTitles(
-                showTitles: false,
-                getTitlesWidget: (value, meta) => Text("Blood sugar Graph"),
-
-              )
-            ),
-
-            bottomTitles: AxisTitles(
-              axisNameWidget: const Text("Number of records"),
-              axisNameSize: 25,
-              sideTitles: SideTitles( 
-                showTitles: true,
-                reservedSize: 30,
-              )
-              
-            )
-            
-          ),
+              )),
+              topTitles: AxisTitles(
+                  axisNameWidget: const Text("Blood Sugar Graph"),
+                  axisNameSize: 35,
+                  sideTitles: SideTitles(
+                    showTitles: false,
+                    getTitlesWidget: (value, meta) => Text("Blood sugar Graph"),
+                  )),
+              bottomTitles: AxisTitles(
+                  axisNameWidget: const Text("Number of records"),
+                  axisNameSize: 25,
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    reservedSize: 30,
+                  ))),
           borderData: FlBorderData(show: true),
           barGroups: records.asMap().entries.map((entry) {
             return BarChartGroupData(
@@ -122,12 +159,10 @@ class _BloodSugarScreenState extends State<BloodSugarScreen> {
                   color: Colors.blueAccent,
                   width: 18,
                   borderRadius: BorderRadius.circular(2),
-                  borderSide:BorderSide(
-                    strokeAlign: BorderSide.strokeAlignCenter,
-                    width: BorderSide.strokeAlignOutside,
-                    color: Colors.black
-                    
-                      ),
+                  borderSide: BorderSide(
+                      strokeAlign: BorderSide.strokeAlignCenter,
+                      width: BorderSide.strokeAlignOutside,
+                      color: Colors.black),
                   backDrawRodData: BackgroundBarChartRodData(
                     show: false,
                     toY: 10,
@@ -148,6 +183,9 @@ class _BloodSugarScreenState extends State<BloodSugarScreen> {
     }
 
     return Card(
+      surfaceTintColor: const Color.fromARGB(255, 245, 246, 248),
+      borderOnForeground: true,
+      semanticContainer: true,
       color: Colors.blue[100],
       margin: EdgeInsets.symmetric(vertical: 8),
       child: Padding(
@@ -158,12 +196,16 @@ class _BloodSugarScreenState extends State<BloodSugarScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('${records[0]['value']} mmol/L',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
+                Text(
+                  '${records[0]['value']} mmol/L',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textHeightBehavior:
+                      TextHeightBehavior(applyHeightToFirstAscent: true),
+                ),
                 Text(records[0]['status']),
               ],
             ),
-            SizedBox(height: 5),
+            SizedBox(height: 15),
             Text(records[0]['date']),
           ],
         ),
@@ -178,15 +220,15 @@ class _BloodSugarScreenState extends State<BloodSugarScreen> {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () {       
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => HomeScreen()),
-          );
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomeScreen()),
+            );
           },
         ),
-        title: Text('Blood Sugar', style: TextStyle(color: Colors.blue)),
+        title: Text('Blood Sugar', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.blueGrey,
         elevation: 0,
       ),
@@ -216,13 +258,17 @@ class _BloodSugarScreenState extends State<BloodSugarScreen> {
                                     child: Padding(
                                       padding: EdgeInsets.all(12),
                                       child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
                                         children: [
                                           Row(
-                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
                                             children: [
                                               Text('${record['value']} mmol/L',
-                                                  style: TextStyle(fontWeight: FontWeight.bold)),
+                                                  style: TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.bold)),
                                               Text(record['status']),
                                             ],
                                           ),
@@ -239,7 +285,6 @@ class _BloodSugarScreenState extends State<BloodSugarScreen> {
                                 child: Text("No other records available"),
                               ),
                       ),
-
                     ElevatedButton(
                       onPressed: _toggleShowAllRecords,
                       style: ElevatedButton.styleFrom(
@@ -266,7 +311,8 @@ class _BloodSugarScreenState extends State<BloodSugarScreen> {
                 backgroundColor: Colors.blue,
                 minimumSize: Size(double.infinity, 50),
               ),
-              child: Text('+ Add Record', style: TextStyle(color: Colors.white)),
+              child:
+                  Text('+ Add Record', style: TextStyle(color: Colors.white)),
             ),
           ),
         ],
